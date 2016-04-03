bd18f20c1a146e83cadc541b26552fa3b3cbd3e7
Adrian Nuyda <adamnuyda@yahoo.com>
2016-04-03 13:18:38 -0700

    first commit w/ signin and signup started

---
+++ README.md
@@ -0,0 +1,5 @@
+# join_me
+
+:tada:
+
+Enjoy… 
---
+++ public/js/app.routes.js
@@ -0,0 +1,33 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .config(appRoutes);
+
+  appRoutes.$inject = ["$stateProvider", "$urlRouterProvider"];
+  function appRoutes($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state("home", {
+        url: "/",
+        templateUrl: "/js/app/layouts/home.html",
+      })
+      .state("signin", {
+        url: "/signin",
+        templateUrl: "/js/app/login/signin.html",
+      })
+      // .state("chatroom", {
+      //   url: "/chatroom", //this might needs it's own ID
+      //   templateUrl: "/js/app/layouts/home.html",
+      // })
+      .state("chatroom_list", {
+        url: "/chatrooms",
+        templateUrl: "/js/app/chatroom/chatrooms.html",
+      });
+
+    $urlRouterProvider.otherwise("/");
+
+  }
+
+})();
