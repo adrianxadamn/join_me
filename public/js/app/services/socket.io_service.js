6ed036cb450c5e83cabc82b1442855e56b58410c
Adrian Nuyda <adamnuyda@yahoo.com>
2016-04-06 19:58:46 -0700

    changes localhost to herokuapp

--- public/js/app/services/socket.io_service.js
+++ public/js/app/services/socket.io_service.js
@@ -8,7 +8,7 @@
   socketService.$inject = [];
 
   function socketService() {
-    var socket = io.connect('http://localhost:3000');
+    var socket = io.connect('https://agile-cove-29324.herokuapp.com/');
 
     return socket;
   }
